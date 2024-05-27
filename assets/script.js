5b8f8328581156ca8163b480d5dad8282e4c6a40
Александр Платонов <100796152+alexandergh1337@users.noreply.github.com>
2024-05-27 23:16:24 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
-// 123
+function toggleForms() {
+    const registrationForm = document.getElementById('registration-form');
+    const loginForm = document.getElementById('login-form');
+    if (registrationForm.classList.contains('block')) {
+        registrationForm.classList.remove('block');
+        registrationForm.classList.add('hidden');
+        loginForm.classList.remove('hidden');
+        loginForm.classList.add('block');
+    } else {
+        registrationForm.classList.remove('hidden');
+        registrationForm.classList.add('block');
+        loginForm.classList.remove('block');
+        loginForm.classList.add('hidden');
+    }
+}
